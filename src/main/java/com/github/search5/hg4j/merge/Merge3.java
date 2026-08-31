97e8b7f0e099122b78be1dcddd3c8d05ff2e7059
Lee Andrew Jiho <search5@gmail.com>
2026-08-31 18:34:33 +0900

    Refactor: Phase 2 - Move Merge3 to merge package

--- src/main/java/com/github/search5/hg4j/merge/Merge3.java
+++ src/main/java/com/github/search5/hg4j/merge/Merge3.java
@@ -1,4 +1,4 @@
-package com.github.search5.hg4j.core;
+package com.github.search5.hg4j.merge;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/test/java/com/github/search5/hg4j/merge/Merge3Test.java
+++ src/test/java/com/github/search5/hg4j/merge/Merge3Test.java
@@ -1,4 +1,4 @@
-package com.github.search5.hg4j.core;
+package com.github.search5.hg4j.merge;
 
 import org.junit.jupiter.api.Test;
 import java.util.List;
